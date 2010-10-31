ff73c4b83af4f7598715c1b3051caacf706bd1a0
Francis M. Tyers <ftyers@prompsit.com>
2010-10-31 22:15:28 +0000

    test stuff

---
+++ dev/inconsistency.sh
@@ -0,0 +1,22 @@
+TMPDIR=/tmp
+
+if [[ $1 = "sc-ca" ]]; then
+
+lt-expand ../apertium-ca-sc.sc.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+        apertium-pretransfer|
+        apertium-transfer ../apertium-ca-sc.sc-ca.t1x  ../sc-ca.t1x.bin  ../sc-ca.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../sc-ca.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+
+elif [[ $1 = "ca-sc" ]]; then
+
+lt-expand ../apertium-ca-sc.ca.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+        apertium-pretransfer|
+        apertium-transfer ../apertium-ca-sc.ca-sc.t1x  ../ca-sc.t1x.bin  ../ca-sc.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../ca-sc.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
+
+
+else
+	echo "sh inconsistency.sh <direction>";
+fi
