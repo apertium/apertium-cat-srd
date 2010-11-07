35f158f5b01d5b43dbbf77729695f71573f0cdf0
Francis M. Tyers <ftyers@prompsit.com>
2010-11-07 02:10:35 +0000

    more

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -2,7 +2,7 @@ TMPDIR=/tmp
 
 if [[ $1 = "sc-ca" ]]; then
 
-lt-expand ../apertium-ca-sc.sc.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-ca-sc.sc.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-ca-sc.sc-ca.t1x  ../sc-ca.t1x.bin  ../sc-ca.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../sc-ca.autogen.bin > $TMPDIR/tmp_testvoc3.txt
@@ -10,7 +10,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "ca-sc" ]]; then
 
-lt-expand ../ca/ca.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../ca/ca.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-ca-sc.ca-sc.t1x  ../ca-sc.t1x.bin  ../ca-sc.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../ca-sc.autogen.bin > $TMPDIR/tmp_testvoc3.txt
