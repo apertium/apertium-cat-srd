11f7e72dc8d3439cd6abb6345d9c0455bdf092b1
Hèctor Alòs i Font <hectoralos@gmail.com>
2017-05-20 19:45:29 +0000

    retocs

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -1,5 +1,5 @@
 echo "==Catalan->Sardinian==========================";
 bash inconsistency.sh ca-sc > /tmp/ca-sc.testvoc; sh inconsistency-summary.sh /tmp/ca-sc.testvoc ca-sc
-echo ""
-echo "==Sardinian->Catalan===========================";
-bash inconsistency.sh sc-ca > /tmp/sc-ca.testvoc; bash inconsistency-summary.sh /tmp/sc-ca.testvoc sc-ca
+#echo ""
+#echo "==Sardinian->Catalan===========================";
+#bash inconsistency.sh sc-ca > /tmp/sc-ca.testvoc; bash inconsistency-summary.sh /tmp/sc-ca.testvoc sc-ca
